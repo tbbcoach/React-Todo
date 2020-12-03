82027dfeb4f688dec40447c8e7827a5f0268305d
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-12-02 19:41:15 -0500

    started adding code to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
 import React from 'react';
 
+const list = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      list
+    };
+  }
+  
+  addItem = (e, item) => {
+    console.log('adding item');
+    e.preventDefault();
+    const newItem = {
+      name: item,
+      completed: false,
+      id: Date.now()
+    };
+    this.setState({
+      ...this.state,
+      list: [...this.state, newItem]
+    });
+  };
+
+  toggleItem = (itemId) => {
+    console.log('toggling item', itemId);
+    this.setState({
+      ...this.state,
+      list: this.state.list.map((item) => {
+        if (itemId === item.id) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      })
+    });
+  };
+  
   render() {
     return (
       <div>
